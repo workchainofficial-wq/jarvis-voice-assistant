abde62bdd77985b6e6f9d3594c76304f0a029f65
SuperAI <superai@jarvis.ai>
2026-02-17 03:05:13 +0000

    Fix MainActivity - remove v1 embedding reference

--- JARVIS_APP/android/app/src/main/kotlin/com/sultanai/jarvis_assistant/MainActivity.kt
+++ JARVIS_APP/android/app/src/main/kotlin/com/sultanai/jarvis_assistant/MainActivity.kt
@@ -3,4 +3,5 @@ package com.sultanai.jarvis_assistant
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    // Flutter v2 embedding - no manual plugin registration needed
 }
